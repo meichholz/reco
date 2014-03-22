c78c27ceff96b3f3fde75868d006f1bd8787ff00
Marian <marian@bugslayer.de>
2014-03-22 11:16:50 +0100

    wip.

--- yardplay/default/class/html/setup.rb
+++ yardplay/default/class/html/setup.rb
@@ -1,7 +1,6 @@
 def init
   super
   # p sections
-  # %w(header box_info pre_docstring subclasses children constant_summary inherited_constants attribute_summary item_summary inherited_attributes method_summary item_summary inherited_methods constructor_details methodmissing attribute_details method_details_list)
   sections.place(:ubbu).after(:box_info)
 end
 
--- yardplay/plugin.rb
+++ yardplay/plugin.rb
@@ -1,3 +1,4 @@
+# wiki: https://mail.ag.freenet.ag/wiki/Tools/Yard
 YARD::Tags::Library.define_tag("Please fix this issue", :FIXME)
 YARD::Tags::Library.define_tag("Please fix this issue", :fixme)
 YARD::Tags::Library.define_tag("Think about this", :think)
