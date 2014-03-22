cf31b67cc3848e19189e277e917c6157b01611ce
Marian <marian@bugslayer.de>
2014-03-22 10:02:25 +0100

    Pfagverküerzung.

--- yardplay/plugin.rb
+++ yardplay/plugin.rb
@@ -6,8 +6,6 @@ YARD::Tags::Library.define_tag("Todo", :todo)
 YARD::Tags::Library.define_tag("Todo", :TODO)
 YARD::Tags::Library.define_tag("Todo List", :todolist)
 
-# hier muesste man was zum Templating abschauen können:
-# https://github.com/lsegal/yard-spec-plugin/blob/master/lib/yard-rspec.rb
-
-YARD::Templates::Engine.register_template_path File.join(File.dirname(__FILE__), 'templates')
+# even the shortest path must contain 'default'
+YARD::Templates::Engine.register_template_path File.dirname(__FILE__)
 
